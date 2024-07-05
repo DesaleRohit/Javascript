16f5f1d490f9bebd8fe2f4b29a4e73c64bc13b6f
DesaleRohit <rohitdesale2005@gmail.com>
2024-07-05 15:39:56 +0530

    [ADDED]:Rohit -Check voting elegibility using Javascript

---
+++ voting.js
@@ -0,0 +1,14 @@
+let  age;
+
+age=prompt("Enter the Age");
+
+if(age>=18)
+{
+    document.write("Allowed for Voting.");
+    console.log("Allowed for Voting.");
+}
+else
+{
+    document.write("Not Allowed for Voting.");
+    console.log("Not Allowed for Voting.");
+}
