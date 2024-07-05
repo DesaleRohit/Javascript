319b031ac6b5b1a07d4bb926c8260a62311c5cd2
DesaleRohit <rohitdesale2005@gmail.com>
2024-07-05 15:37:30 +0530

    [ADDED]:Rohit -Find Leap year using Javascript

---
+++ leapyear.js
@@ -0,0 +1,13 @@
+let year = 0;
+year =prompt("Enter the Year");
+
+if(year % 4 == 0)
+{
+    console.log( year + " is leap Year  ");
+    document.write( year + " is leap Year ");
+}
+else
+{
+    console.log( year +" is Not Leap Year");
+    document.write( year + " is Not leap Year ");
+}
