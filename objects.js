a4a369c42c8af5805c722a2f529a2c9a694ab4ee
DesaleRohit <rohitdesale2005@gmail.com>
2025-04-11 09:16:11 +0530

    [ADDED]:Rohit - Object demo

---
+++ objects.js
@@ -0,0 +1,8 @@
+let A = {
+    "name": "Rohit",
+    "age": 21,
+    "gender": "male",
+    "value": true,
+}
+console.log(A);
+console.log(typeof A.name , typeof A.age , typeof A.gender , typeof A.value);
