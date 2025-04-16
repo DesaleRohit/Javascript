4078c3483abff03516a36b50a7c2098623e56735
DesaleRohit <rohitdesale2005@gmail.com>
2025-04-16 14:55:38 +0530

    [ADDED]:Rohit - Object demo 4

---
+++ objects4.js
@@ -0,0 +1,10 @@
+const object1 = {
+    message: 'Good job',
+    fun() {
+        console.log('Hello Boss'); //Shorthand Method to create function in object without property name
+    }
+};
+
+
+console.log(object1);
+object1.fun();
